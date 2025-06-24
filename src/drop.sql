c124035f4da08810917709074b3cda8fd9d72fc3
Kosta P <kapodgaets@gmail.com>
2025-06-24 08:43:08 +0300

    add updated sql queries for migrations

--- src/drop.sql
+++ src/drop.sql
@@ -3,3 +3,5 @@ drop table if exists "__EFMigrationsHistory" cascade;
 drop table if exists "Citizens" cascade;
 
 drop table if exists "Users" cascade;
+
+drop table if exists "CallcenterCases" cascade;
